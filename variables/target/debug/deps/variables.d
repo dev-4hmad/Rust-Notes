330e6a2fbaeb0b18c181f67a38c469d1005943c6
Muhammad Ahmad <drevex88@gmail.com>
2025-08-18 20:23:24 +0500

    Stop tracking target folder

--- variables/target/debug/deps/variables.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Ahmad\Desktop\Rust\01_RUST\variables\target\debug\deps\variables.d: src\main.rs
-
-C:\Users\Ahmad\Desktop\Rust\01_RUST\variables\target\debug\deps\variables.exe: src\main.rs
-
-src\main.rs:
--- variables/target/debug/variables.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Ahmad\Desktop\Rust\01_RUST\variables\target\debug\variables.exe: C:\Users\Ahmad\Desktop\Rust\01_RUST\variables\src\main.rs
